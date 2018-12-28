33ec592a8c6262e718f6b20bd9911bcbfb5932a0
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-12-28 22:49:29 +0100

    pkgmgr: better helper

--- helpers/up.sh
+++ helpers/up.sh
@@ -2,23 +2,26 @@
 DISTRIB_ID=
 DISTRIB_CODENAME=
 DISTRIB_RELEAASE=
-oldubuntu="^(10\.|12\.|13\.14.10|15\.|16.10|17\.)"
+oldubuntu="^(10\.|12\.|13\.|14.10|15\.|16.10|17\.)"
 if [ -e /etc/lsb-release ];then
     . /etc/lsb-release
 fi
-if ( echo $DISTRIB_ID|egrep -iq "mint|ubuntu" );then
+if ( echo $DISTRIB_ID | egrep -iq "mint|ubuntu" );then
     if ( echo $DISTRIB_RELEASE |egrep -iq $oldubuntu);then
-        sed -i -r \
-            -e 's!archive.ubuntu.com!old-releases.ubuntu.com!g' \
-            $(find /etc/apt/sources.list* -type f)
+        sed -i -r -e 's!archive.ubuntu.com!old-releases.ubuntu.com!g' \
+            $( find /etc/apt/sources.list* -type f; )
     fi
 fi
-: "install packages" \
-    &&
-    DO_UPDATE="1" \
-    WANTED_PACKAGES=$(grep -vE "^\s*#" packages.txt  | tr "\n" " ") \
-    ./cops_pkgmgr_install.sh \
-    && for i in gpg gnupg;do if ( ./cops_pkgmgr_install.sh $i );then break;fi;done \
-    && gpg --version &>/dev/null
-wait
+install_gpg() {
+    for i in gpg gnupg
+    do
+        if ( ./cops_pkgmgr_install.sh $i ) then
+            break
+        fi
+    done
+    gpg --version &>/dev/null
+}
+DO_UPDATE="1" \
+WANTED_PACKAGES="$(grep -vE '^\s*#' packages.txt  | tr "\n" ' '; )" \
+    ./cops_pkgmgr_install.sh && install_gpg
 # vim:set et sts=4 ts=4 tw=0:
