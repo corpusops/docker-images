702dc2a4ad6d6428b271995e285cf4857b063491
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-12-28 01:26:14 +0100

    Fix pkgs installers sync

--- helpers/add_opt_pkgs.sh
+++ helpers/add_opt_pkgs.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 : "install optional packages" \
     && pkgs=$(grep -vE "^\s*#" optional_packages.txt  | tr "\n" " ") \
-    && DO_UPDATE="" WANTED_PACKAGES="" WANTED_EXTRA_PACKAGES="$pkgs" \
-     ./cops_pkgmgr_install.sh
+    && \
+    DO_UPDATE="" \
+    WANTED_PACKAGES="" \
+    WANTED_EXTRA_PACKAGES="$pkgs" \
+    ./cops_pkgmgr_install.sh
+wait
 # vim:set et sts=4 ts=4 tw=0:
--- helpers/up.sh
+++ helpers/up.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 : "install packages" \
-    &&  DO_UPDATE="1" \
-    ./cops_pkgmgr_install.sh $(grep -vE "^\s*#" packages.txt  | tr "\n" " ") \
-    && export DO_UPDATE="" \
+    &&
+    DO_UPDATE="1" \
+    WANTED_PACKAGES=$(grep -vE "^\s*#" packages.txt  | tr "\n" " ") \
+    ./cops_pkgmgr_install.sh \
     && for i in gpg gnupg;do if ( ./cops_pkgmgr_install.sh $i );then break;fi;done \
-    && gpg --version &>/dev/null \
-#
+    && gpg --version &>/dev/null
+wait
 # vim:set et sts=4 ts=4 tw=0:
