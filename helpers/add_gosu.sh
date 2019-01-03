ccf7d230b3237bb1dbd50af299559f3a057cef08
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-01-03 17:06:43 +0100

    Remove one gpg server

--- helpers/add_gosu.sh
+++ helpers/add_gosu.sh
@@ -4,7 +4,7 @@ GITHUB_PAT="${GITHUB_PAT:-$(echo 'OGUzNjkwMDZlMzNhYmNmMGRiNmE5Yjg1NWViMmJkNWVlNj
 GOSU_RELEASE="${GOSU_RELEASE:-latest}"
 CURL_SSL_OPTS="${CURL_SSL_OPTS:-"--tlsv1"}"
 GOSU_GPG_KEYS="${GOSU_GPG_KEYS:-B42F6819007F00F88E364FD4036A9C25BF357DD4}"
-GPG_KEYS_SERVERS="${GPG_KEYS_SERVERS:-"https://pgp.key-server.io/ hkp://p80.pool.sks-keyservers.net:80 hkp://ipv4.pool.sks-keyservers.net hkp://keyserver.ubuntu.com:80 hkp://pgp.mit.edu:80"}"
+GPG_KEYS_SERVERS="${GPG_KEYS_SERVERS:-"hkp://p80.pool.sks-keyservers.net:80 hkp://ipv4.pool.sks-keyservers.net hkp://keyserver.ubuntu.com:80 hkp://pgp.mit.edu:80"}"
 do_curl() { if ! ( curl "$@" );then curl $CURL_SSL_OPTS "$@";fi; }
 install() {
     if [ "x${SDEBUG}" != "x" ];then set -x;fi
