82b66aeeeac045cd28a2a0df08c78d4453a52a74
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-01-03 17:22:40 +0100

    gosu helper: more retries on gpg servers

--- helpers/add_gosu.sh
+++ helpers/add_gosu.sh
@@ -16,11 +16,14 @@ install() {
     && arch=$( uname -m|sed -re "s/x86_64/amd64/g" ) \
     && : one keyserver may fail, try on multiple servers \
     && for k in $GOSU_GPG_KEYS;do \
-        touch /k_$k \
-        && for s in $GPG_KEYS_SERVERS;do \
-          if ( gpg --batch --keyserver $s --recv-keys $k );then \
-            rm -f /k_$k && break;else echo "Keyserver failed: $s" >&2;fi;done \
-        && if [ -e /k_$k ];then exit 1;fi \
+        touch /tmp/gosu_k_$k \
+        && set -x&&for retry in $(seq 4)
+           do if [ -e /tmp/gosu_k_$k ];then for s in $GPG_KEYS_SERVERS;do \
+             if ( gpg --batch --keyserver $s --recv-keys $k ) \
+             then rm -f /tmp/gosu_k_$k && break; \
+             else echo "Keyserver failed: $s/$k (retry: $retry)" >&2;fi
+           done;fi;done \
+        && if [ -e /tmp/gosu_k_$k ];then echo "Key failed: $k" >&2 && exit 1;fi; \
        done \
     && urls="$(do_curl -s -H "Authorization: token $GITHUB_PAT" \
         "https://api.github.com/repos/tianon/gosu/releases/$GOSU_RELEASE" \
