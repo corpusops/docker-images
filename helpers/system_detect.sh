6dd5be7f2ed7555ff7a842fad34a6e4eb0fbe3d0
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-01-09 00:15:32 +0100

    fix detector for suse

--- helpers/system_detect.sh
+++ helpers/system_detect.sh
@@ -6,8 +6,8 @@ elif ( egrep -iq "debian|mint|ubuntu" /etc/*-release 2>/dev/null);then
     system=apt
 elif ( egrep -iq "red.?hat" /etc/*-release 2>/dev/null);then
     system=redhat
-elif ( egrep -iq "suse" /etc/*-release 2>/dev/null);then
+elif ( egrep -iq "suse" $(find /etc/ImageVersion /etc/*-release||/bin/true 2>/dev/null) 2>/dev/null);then
     system=suse
 fi
 echo $system
-# vim:set et sts=4 ts=4 tw=80:
+# vim:set et sts=4 ts=4 tw=0:
