9852f4b0cfde9f850a34df6bd1056029d1981384
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-01-10 18:52:31 +0100

    fix suse detector

--- helpers/system_detect.sh
+++ helpers/system_detect.sh
@@ -2,12 +2,12 @@
 system=generic
 if [ -e /etc/alpine-release ];then
     system=alpine
+elif ( egrep -iq "suse" $(find /etc/ImageVersion /etc/*-release||/bin/true 2>/dev/null) 2>/dev/null);then
+    system=suse
 elif ( egrep -iq "debian|mint|ubuntu" /etc/*-release 2>/dev/null);then
     system=apt
 elif ( egrep -iq "fedora|centos|ol|oracle|red.?hat" /etc/*-release 2>/dev/null);then
     system=redhat
-elif ( egrep -iq "suse" $(find /etc/ImageVersion /etc/*-release||/bin/true 2>/dev/null) 2>/dev/null);then
-    system=suse
 fi
 echo $system
 # vim:set et sts=4 ts=4 tw=0:
