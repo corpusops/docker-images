c28ac5db333a0a72b99819c1b2776b58cd4dc3e5
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-01-16 09:42:11 +0100

    Make frep the nginx config preprocessor

--- rootfs/bin/nginx.sh
+++ rootfs/bin/nginx.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 set -e
-export NGINX_CONF_PREFIX="${NGINX_CONF_PREFIX:-${CONF_PREFIX:-NGINX_}}"
-get_conf_vars() {
-    echo $( env | egrep "${CONF_PREFIX}[^=]+=.*" \
-            | sed -re "s/((${CONF_PREFIX})[^=]+)=.*/$\1;/g";); }
 SDEBUG=${SDEBUG-}
 if [ "x$SDEBUG" != "x" ];then set -x;fi
 NO_CHOWN=${NO_CHOWN-}
@@ -19,14 +15,17 @@ export NGINX_LOGS_DIR="${NGINX_LOGS_DIR:-"/var/log/nginx"}"
 export NGINX_LOGS_DIRS="/logs /log $NGINX_LOGS_DIR"
 export NGINX_SOCKET_DIR="$(dirname "$NGINX_SOCKET_PATH")"
 export NGINX_BIN=${NGINX_BIN:-"nginx"}
-export NGINX_CONFIGS="${NGINX_CONFIGS-"$( find $NGINX_CONF_DIR -type f|egrep -v '\.template$')
+export NGINX_FREP_SKIP=${NGINX_FREP_SKIP:-"(\.skip|\.skipped)$"}
+export NGINX_CONFIGS="${NGINX_CONFIGS-"$( \
+    find $NGINX_CONF_DIR -type f \
+    |egrep -v "$NGINX_FREP_SKIP|\.template$")
 /etc/logrotate.d/nginx"}"
 if [[ -z $NGINX_STD_OUTPUT ]];then rm -fv $NGINX_LOGS_DIR/*;fi
 for e in $NGINX_LOGS_DIRS $NGINX_CONF_DIR;do
     if [ ! -e "$e" ];then mkdir -p "$e";fi
     if [ "x$NO_CHOWN" != "x" ] && [ -e "$e" ];then chown "$NGINX_USER" "$e";fi
 done
-CONF_PREFIX="$NGINX_CONF_PREFIX" confenvsubst.sh $NGINX_CONFIGS
+for i in $NGINX_CONFIGS;do frep $i:$i --overwrite;done
 chmod 600 /etc/logrotate.d/nginx
 exec $NGINX_BIN "$@"
 # vim:set et sts=4 ts=4 tw=80:
