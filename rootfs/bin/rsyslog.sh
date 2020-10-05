63429b9ceae4f0393a770c2cedb97beba3f80c96
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-10-05 21:41:24 +0200

    Add a way to skip rsyslog conf

--- rootfs/bin/rsyslog.sh
+++ rootfs/bin/rsyslog.sh
@@ -2,9 +2,10 @@
 set -e
 RSYSLOGD_BIN=${RSYSLOGD_BIN:-"rsyslogd"}
 RSYSLOGD_ARGS=${RSYSLOGD_ARGS:-"-n"}
+NO_DEFAULT_RSYSLOG_CONF=${NO_DEFAULT_RSYSLOG_CONF-}
 SDEBUG=${SDEBUG-}
 if [ "x$SDEBUG" != "x" ];then set -x;fi
-if [ -e /etc/rsyslog.conf.frep ];then
+if [ "x${NO_DEFAULT_RSYSLOG_CONF}" = "x" ] && [ -e /etc/rsyslog.conf.frep ];then
     frep --overwrite /etc/rsyslog.conf.frep:/etc/rsyslog.conf
 fi
 exec $RSYSLOGD_BIN $RSYSLOGD_ARGS
