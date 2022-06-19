6228ec9b4800f0ee26e3c9e5cc8da893d71a3f58
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2022-06-19 22:47:09 +0200

    new rsyslog conf

--- rootfs/bin/rsyslog.sh
+++ rootfs/bin/rsyslog.sh
@@ -8,5 +8,10 @@ if [ "x$SDEBUG" != "x" ];then set -x;fi
 if [ "x${NO_DEFAULT_RSYSLOG_CONF}" = "x" ] && [ -e /etc/rsyslog.conf.frep ];then
     frep --overwrite /etc/rsyslog.conf.frep:/etc/rsyslog.conf
 fi
+for i in $(ls /etc/rsyslog.d/*.conf.frep 2>/dev/null || true);do
+    if (grep -q -- {{ "$i" );then
+        frep "$i:$(basename $i .frep)" --overwrite
+    fi
+done
 exec $RSYSLOGD_BIN $RSYSLOGD_ARGS
 # vim:set et sts=4 ts=4 tw=80:
