14cf2775079f3598c872f29ce0c6633b62f137e7
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2023-12-10 22:47:09 +0100

    add infest helper

---
+++ rootfs/bin/infest.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+set -e
+# to be added inside vanilla images to infest from corpusops image mounted via buildkit in /s prefix
+NO_PKGS_INSTALL=${NO_PKGS_INSTALL-}
+NO_HELPERS_SYNC=${NO_HELPERS_SYNC-}
+# assuming we are in $prefix/bin
+if [ "x${SDEBUG-}" != "x" ];then set -x;fi
+T=$(cd $(dirname $(readlink -f $0))/.. && pwd)
+log() { echo $@ >&2; }
+pkgs=""
+if [ "x$NO_PKGS_INSTALL" = "x" ];then
+    for i in logrotate rsync rsyslog curl openssh-client;do
+        if ! ( $i --version &>/dev/null );then
+            log "installing $i"
+            pkgs="$pkgs $i"
+        fi
+    done
+    if ( apk --version &>/dev/null );then
+        pkgs="$pkgs dcron"
+    else
+        pkgs="$pkgs cron"
+    fi
+    if [ "x$pkgs" != "x" ];then
+       WANTED_EXTRA_PACKAGES="$pkgs" $T/bin/cops_pkgmgr_install.sh
+    fi
+fi
+if [ "x$NO_HELPERS_SYNC" = "x" ] && ! ( echo $T | egrep -q "^/$" );then
+    for i in \
+        etc/rsyslog.d/ \
+        etc/supervisor.d/ \
+        etc/logrotate.d/ \
+        bin/cron.sh \
+        bin/rsyslog.sh \
+        cops_helpers/
+        do
+            rsync -aAHv --numeric-ids $i /$i
+        done
+fi
+# vim:set et sts=4 ts=4 tw=0:
